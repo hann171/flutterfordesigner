a5a2315c93fa42c16a3cc6616797dbd0e414b587
hann171 <hannw171@outlook.com>
2021-02-04 02:53:13 +0700

    Chatty complex design

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:chaty/homePage.dart';
 
+import 'homePage.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -10,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
       home: HomePage(),
     );
   }
